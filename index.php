85fd7854e26383e9a4c8b8505aadc47805e5df82
petarpetrovt <petarpetrovt@gmail.com>
2014-12-15 11:49:46 +0200

    * added tags

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require "inc/Database.php";
 $db = new Database();
-$title =  "Blog | Home";
+$title = "Blog | Home";
 $styleFile = "styles/style.css";
 $scriptFile = "scripts/script.js";
 require "inc/header.php";
@@ -9,21 +9,26 @@ require "inc/header.php";
 $posts = $db->getAllPosts();
 
 ?>
-<div class="posts">
-    <?php
-        if(count($posts) > 0){
-            foreach($posts as $row){
+    <div class="posts">
+        <?php
+        if (count($posts) > 0) {
+            foreach ($posts as $row) {
                 echo '<div class="post">';
-                echo '<div class="date clear">'.date('d.m.Y H:i', $row['time']).'</div>';
-                echo '<h3 class="postTitle"><a href="post.php?id='.$row['id'].'" >'.$row['title'].'</a></h3>';
-                echo '<div class="postContent">'.nl2br($row['text']).'</div>';
+                echo '<div class="date clear">' . date('d.m.Y H:i', strtotime($row['time'])) . '</div>';
+                echo '<h3 class="postTitle"><a href="post.php?id=' . $row['id'] . '" >' . $row['title'] . '</a></h3>';
+                echo '<div class="postContent">' . nl2br($row['text']) . '</div>';
+                echo '<div class="tags">Tags: ';
+                $tags = explode('#', $row['tags']);
+                for ($i = 1; $i < count($tags); $i++)
+                    echo '<a class="tag" href="javascript:;">#' . $tags[$i] . ' </a>';
+                echo '</div>';
                 echo '</div>';
             }
-        }else{
+        } else {
             echo '<p>No data</p>';
         }
-    ?>
-</div>
+        ?>
+    </div>
 <?php
 
 require "inc/Footer.php";
