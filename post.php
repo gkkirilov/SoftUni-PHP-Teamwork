1dbf81fda213a4b94002b4c1397691177da7c799
AlexanderMarinov894 <alexander.mrnv@gmail.com>
2014-12-15 15:16:10 +0200

    -Fixed posts preview page bug

--- post.php
+++ post.php
@@ -5,6 +5,7 @@ $title =  "Blog | Posts";
 $styleFile = "styles/style.css";
 $scriptFile = "scripts/script.js";
 require "inc/header.php";
+require "inc/utils.php";
 $posts = $db->getAllPosts();
 ?>
 
@@ -15,7 +16,19 @@ $pattern = '/\?id=[0-9]+/';
 preg_match_all($pattern, $url, $id);
 $id = $id[0][0];
 $id = intval(preg_replace('/\?id=/', '', $id));
+foreach ($posts as $row) {
+    $_SESSION['row'.$row['id']] = $row;
+}
+
 $row = $_SESSION['row'.$id];
+if (isset($_GET["tag"])) {
+    $tag = strip_tags($_GET["tag"]);
+    $posts = $db->searchByTag($tag);
+    echo ' <div class="filter" >Posts filtered by tag: ' . $tag . '<a href="index.php">b</a></div > ';
+} else {
+    $posts = $db->getAllPosts();
+}
+
 
 echo '<div class="post">';
 echo '<div class="date clear">' . date('d.m.Y H:i', strtotime($row['time'])) . '</div>';
@@ -23,8 +36,10 @@ echo '<h3 class="postTitle"><a href="post.php?id=' . $row['id'] . '" >' . $row['
 echo '<div class="postContent">' . nl2br($row['text']) . '</div>';
 echo '<div class="tags">Tags: ';
 $tags = explode('#', $row['tags']);
-for ($i = 1; $i < count($tags); $i++)
-    echo '<a class="tag" href="javascript:;">#' . $tags[$i] . ' </a>';
+for ($i = 1; $i < count($tags); $i++) {
+    $tag = trim($tags[$i]);
+    echo '<a class="tag" href="' . getSearchUrl($tag) . '">#' . $tag . ' </a>';
+}
 echo '</div>';
 echo '</div>';
 ?>
