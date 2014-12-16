66c5128a0a5450474ce364280714527eacf1e20a
Georgi Kirilov <gkkirilov@gmail.com>
2014-12-16 15:39:45 +0200

    updated

--- about.php
+++ about.php
@@ -6,23 +6,38 @@ getHeader("Blog | About");
 <div class="posts">
     <div class="post">
         <h3 class="postTitle">Alexander Marinov</h3>
-        <div class="postContent"></div>
+        <div class="postContent">
+            <article></article>
+            <img src="img/Untitled-1.jpg" alt="photo"/>
+        </div>
     </div>
     <div class="post">
         <h3 class="postTitle">Petar Petrov</h3>
-        <div class="postContent"></div>
+        <div class="postContent">
+            <article></article>
+            <img src="img/Untitled-1.jpg" alt="photo"/>
+        </div>
     </div>
     <div class="post">
         <h3 class="postTitle">Dimitar Hadzhiev</h3>
-        <div class="postContent"></div>
+        <div class="postContent">
+            <article></article>
+            <img src="img/Untitled-1.jpg" alt="photo"/>
+            </div>
     </div>
     <div class="post">
         <h3 class="postTitle">Ivan Hristov</h3>
-        <div class="postContent"></div>
+        <div class="postContent">
+            <article></article>
+            <img src="img/Untitled-1.jpg" alt="photo"/>
+            </div>
     </div>
     <div class="post">
         <h3 class="postTitle">Georgi Kirilov</h3>
-        <div class="postContent"></div>
+        <div class="postContent">
+            <article></article>
+            <img src="img/Untitled-1.jpg" alt="photo"/>
+            </div>
     </div>
 </div>
 <?php
