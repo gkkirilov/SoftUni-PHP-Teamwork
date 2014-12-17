dcb7dc3a33d1cb8f7483f1af28ec0e35c4ae70c4
petarpetrovt <petarpetrovt@gmail.com>
2014-12-17 10:46:59 +0200

    * Fixed getUrl function;

--- inc/utils.php
+++ inc/utils.php
@@ -2,5 +2,9 @@
 
 function getSearchUrl($file)
 {
-    return 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/' . $file;
+    $dir = dirname($_SERVER['REQUEST_URI']);
+    if (strpos($dir, 'admin') !== false)
+        return 'http://' . $_SERVER['HTTP_HOST'] . dirname(dirname($_SERVER['REQUEST_URI'])) . '/' . $file;
+    else
+        return 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/' . $file;
 }
